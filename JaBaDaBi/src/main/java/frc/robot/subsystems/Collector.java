ea5e957b911fa6850906f9d05f047cadd5d9ab41
Apolive <lilphokiss@gmail.com>
2026-01-22 16:35:46 -0600

    more stuff for collector

--- JaBaDaBi/src/main/java/frc/robot/subsystems/Collector.java
+++ JaBaDaBi/src/main/java/frc/robot/subsystems/Collector.java
@@ -1,3 +1,41 @@
 package frc.robot.subsystems;
 
-public class Collector extends SubsystemBase
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Collector extends SubsystemBase {
+
+    private SparkMax dCollectorMotor;
+    private SparkMax iCollectorMotor;
+
+    private SparkMaxConfig dCollectMotorConfig;
+    private SparkMaxConfig iCollectMotorConfig;
+    private SparkClosedLoopController iCollectorM;
+
+    public Collector() {
+
+        dCollectorMotor = new SparkMax(34, MotorType.kBrushless);
+        iCollectorMotor = new SparkMax(35, MotorType.kBrushless);
+
+        dCollectMotorConfig = new SparkMaxConfig();
+        dCollectMotorConfig.idleMode(IdleMode.kBrake);
+        dCollectMotorConfig.smartCurrentLimit(40);
+        dCollectMotorConfig.inverted(false);
+        iCollectMotorConfig = new SparkMaxConfig();
+        iCollectMotorConfig.follow(34, true);
+
+        dCollectorMotor.configure(dCollectMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        iCollectorMotor.configure(iCollectMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        iCollectorM= iCollectorMotor.getClosedLoopController();
+
+    }
+
+}

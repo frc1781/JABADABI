969a1002556a80c0c5e902a30552b55e58fee288
BryceH127 <bkharris5@cps.edu>
2026-01-30 16:36:42 -0600

    changing angle of back camera

--- JaBaDaBi/src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ JaBaDaBi/src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -387,7 +387,7 @@ static AprilTagFieldLayout loadAprilTagField() {
               VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
                
     BACK_CAM("Back",
-               new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(17), Units.degreesToRadians(180)),
+               new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(23.0), Units.degreesToRadians(180)),
                new Translation3d(Units.inchesToMeters(13.8125), Units.inchesToMeters(0), Units.inchesToMeters(16.5)),
                VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
  

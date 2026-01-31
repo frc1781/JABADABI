184e45df5d6e857259a75be8e29c05396c931021
BrazierJhaMari <jebrazier@cps.edu>
2026-01-31 12:37:25 -0600

    Set back button as zeroGyro

--- JaBaDaBi/src/main/java/frc/robot/RobotContainer.java
+++ JaBaDaBi/src/main/java/frc/robot/RobotContainer.java
@@ -113,14 +113,19 @@ public class RobotContainer {
     drivebase.setDefaultCommand(driveFieldOriented);
     lights.setDefaultCommand(lights.set(Lights.Special.OFF));
 
-    driverXbox.a().whileTrue(new Climb(lights));
-    driverXbox.x().whileTrue(new Collect(lights));
+    driverXbox.a().whileTrue(collector.collect(() -> 0));// put collect in here later
+    driverXbox.b().whileTrue(collector.collect(() -> 0)); // invert floor intake here later
+    driverXbox.x().whileTrue(Commands.none());
+    driverXbox.y().whileTrue(Commands.none());
     driverXbox.start().onTrue((Commands.runOnce(drivebase::zeroGyro)));
-    driverXbox.back().whileTrue(Commands.none());
+    driverXbox.back().onTrue(Commands.runOnce(drivebase::zeroGyro));
     driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
     driverXbox.rightBumper().onTrue(Commands.none());
-    driverXbox.y().whileTrue(new DriveToPose(lights));
-    driverXbox.b().whileTrue(new Shoot(lights));
+    driverXbox.povUp().whileTrue(new Climb(lights)); //Climb up
+    driverXbox.povDown().whileTrue(new Climb(lights)); //Climb down
+    driverXbox.leftTrigger().whileTrue(new DriveToPose(lights)); // drives to hub or somewhere close to hub
+   driverXbox.rightTrigger().whileTrue(new Shoot(lights)); // Shoot
+
   }
 
   public Command getAutonomousCommand() {

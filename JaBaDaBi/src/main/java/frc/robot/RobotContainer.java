10fe071a5a16cbe54082607f967414844893d802
BrazierJhaMari <jebrazier@cps.edu>
2026-02-02 16:38:21 -0600

    Changed shoot and climb to use shooter and climber code

--- JaBaDaBi/src/main/java/frc/robot/RobotContainer.java
+++ JaBaDaBi/src/main/java/frc/robot/RobotContainer.java
@@ -135,9 +135,9 @@ public class RobotContainer {
     driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
     driverXbox.rightBumper().onTrue(Commands.none());
     copilotXbox.leftTrigger().whileTrue(new DriveToPose(lights)); // drives to hub or somewhere close to hub / aim
-    copilotXbox.rightTrigger().whileTrue(new Shoot(lights)); // Shoot
-    copilotXbox.povUp().whileTrue(new Climb(lights)); //Climb up
-    copilotXbox.povDown().whileTrue(new Climb(lights)); //Climb down
+    copilotXbox.rightTrigger().whileTrue(shooter.shoot(() -> 0)); // Shoot
+    copilotXbox.povUp().whileTrue(climber.ascend().repeatedly()); //Climb up
+    copilotXbox.povDown().whileTrue(climber.descend().repeatedly()); //Climb down
   }
 
   public Command getAutonomousCommand() {

8286c70b41d1ef0261aa9fe1cc32b67ea9c08b91
Aditya <asingharoy@cps.edu>
2026-02-24 17:17:19 -0600

    changed climber to velocity control (pending testing)

--- JaBaDaBi/src/main/java/frc/robot/subsystems/Climber.java
+++ JaBaDaBi/src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -13,21 +12,24 @@ import org.littletonrobotics.junction.Logger;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class Climber extends SubsystemBase {
 
-    //REAL MOTOR
+    // REAL MOTOR
     private SparkMax motorLeft;
     private SparkMax motorRight;
 
     private RelativeEncoder motorLeftEncoder;
     private RelativeEncoder motorRightEncoder;
 
-    private PIDController pidController;
+    private SparkClosedLoopController motorLeftController;
+    private SparkClosedLoopController motorRightController;
 
     public Climber() {
         motorLeft = new SparkMax(Constants.Climber.MOTOR_LEFT, SparkLowLevel.MotorType.kBrushless);
@@ -35,8 +37,8 @@ public class Climber extends SubsystemBase {
 
         SparkMaxConfig motorConfigLeft = new SparkMaxConfig();
         motorConfigLeft.idleMode(SparkBaseConfig.IdleMode.kBrake);
-        motorConfigLeft.inverted(true); //idfk the inversion
-        motorConfigLeft.encoder.positionConversionFactor(Constants.Climber.INCHES_PER_REVOLUTION);
+        motorConfigLeft.inverted(true); // idfk the inversion
+        motorConfigLeft.encoder.velocityConversionFactor(Constants.Climber.INCHES_PER_REVOLUTION);
         motorConfigLeft.closedLoop.apply(Constants.Climber.CLOSED_LOOP_CONFIG);
         motorConfigLeft.smartCurrentLimit(40);
         motorLeft.configure(motorConfigLeft, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
@@ -44,16 +46,16 @@ public class Climber extends SubsystemBase {
         SparkMaxConfig motorConfigRight = new SparkMaxConfig();
         motorConfigRight.idleMode(SparkBaseConfig.IdleMode.kBrake);
         motorConfigRight.inverted(true);
-        //motorConfigRight.follow(Constants.Climber.MOTOR_LEFT, true); //idfk the inversion
-        motorConfigLeft.closedLoop.apply(Constants.Climber.CLOSED_LOOP_CONFIG);
-        motorConfigRight.encoder.positionConversionFactor(Constants.Climber.INCHES_PER_REVOLUTION);
+        motorConfigRight.encoder.velocityConversionFactor(Constants.Climber.INCHES_PER_REVOLUTION);
+        motorConfigRight.closedLoop.apply(Constants.Climber.CLOSED_LOOP_CONFIG);
         motorConfigRight.smartCurrentLimit(40);
         motorRight.configure(motorConfigRight, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         motorLeftEncoder = motorLeft.getEncoder();
         motorRightEncoder = motorRight.getEncoder();
 
-        pidController = new PIDController(Constants.Climber.P, Constants.Climber.I, Constants.Climber.D);
+        motorLeftController = motorLeft.getClosedLoopController();
+        motorRightController = motorRight.getClosedLoopController();
     }
 
     @Override
@@ -66,22 +68,22 @@ public class Climber extends SubsystemBase {
 
     public Command ascend() {
         return new InstantCommand(() -> {
-            motorLeft.set(pidController.calculate(motorLeftEncoder.getPosition(), 0));
-            motorRight.set(pidController.calculate(motorRightEncoder.getPosition(), 0));
+            motorLeftController.setSetpoint(-1, ControlType.kVelocity);
+            motorRightController.setSetpoint(-1, ControlType.kVelocity);
         }, this).repeatedly();
     }
 
     public Command descend() {
         return new InstantCommand(() -> {
-            motorLeft.set(pidController.calculate(motorLeftEncoder.getPosition(), 21));
-            motorRight.set(pidController.calculate(motorRightEncoder.getPosition(), 21));
+            motorLeftController.setSetpoint(1, ControlType.kVelocity);
+            motorRightController.setSetpoint(1, ControlType.kVelocity);
         }, this).repeatedly();
     }
 
-        public Command idle() {
+    public Command idle() {
         return new RunCommand(() -> {
-            motorLeft.setVoltage(0);;
-            motorRight.setVoltage(0);
+            motorLeftController.setSetpoint(0, ControlType.kVelocity);
+            motorRightController.setSetpoint(0, ControlType.kVelocity);
         }, this);
     }
 }

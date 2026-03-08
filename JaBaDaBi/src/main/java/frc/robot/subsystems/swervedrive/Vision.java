3438def5c8ee1c3e8ff638c7e92daba366959ebb
Aditya Singharoy <asingharoy@cps.edu>
2026-03-08 10:44:18 -0500

    ComeBack paths

--- JaBaDaBi/src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ JaBaDaBi/src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -664,10 +664,9 @@ public class Vision
             estStdDevs = multiTagStdDevs;
           }
           // Increase std devs based on (average) distance
-          if (numTags == 1 && avgDist > 3)
+          if (numTags == 1 && avgDist > 4)
           {
             estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-            System.out.println("test hit this");
           } else
           {
             estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));

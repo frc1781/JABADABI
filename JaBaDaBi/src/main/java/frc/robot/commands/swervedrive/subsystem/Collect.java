35926721eb22c09399dfe1d85ce45d530fe3428d
Aditya Singharoy <asingharoy@cps.edu>
2026-03-20 12:29:05 -0500

    humongous changes to code structure, I hope auto works with this structure

--- JaBaDaBi/src/main/java/frc/robot/commands/swervedrive/subsystem/Collect.java
+++ JaBaDaBi/src/main/java/frc/robot/commands/swervedrive/subsystem/Collect.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.swervedrive.auto;
+package frc.robot.commands.swervedrive.subsystem;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
--- JaBaDaBi/src/main/java/frc/robot/commands/swervedrive/subsystem/ExampleCommand.java
+++ JaBaDaBi/src/main/java/frc/robot/commands/swervedrive/subsystem/ExampleCommand.java
@@ -1,29 +1,33 @@
-package frc.robot.commands.swervedrive.auto;
+package frc.robot.commands.swervedrive.subsystem;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.NeonLights;
 
-public class DriveToPose extends Command {
-
+public class ExampleCommand extends Command {
+    Timer timer;
     NeonLights lights;
 
-    public DriveToPose(NeonLights lights) {
+    public ExampleCommand(NeonLights lights) {
         this.lights = lights;
+        timer = new Timer();
     }
 
     @Override
     public void initialize() {
         System.out.println(getName() + "started");
+        timer.reset();
+        timer.start();
     }
 
     @Override
     public void execute() {
-        lights.set(new NeonLights.Pattern[]{NeonLights.Pattern.FIRE_GRADIENT, NeonLights.Pattern.TRAVEL});
+        lights.set(new NeonLights.Pattern[]{NeonLights.Pattern.GREEN, NeonLights.Pattern.BLINK});
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return timer.get() > 5;
     }
 
     @Override
@@ -33,5 +37,7 @@ public class DriveToPose extends Command {
         } else {
             System.out.println(getName() + "ended");
         }
+        timer.stop();
+        timer.reset();
     }
 }

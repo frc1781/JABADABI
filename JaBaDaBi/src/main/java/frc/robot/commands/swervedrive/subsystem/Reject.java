c869877db329be371277fc5e49493b369272177f
Aditya Singharoy <asingharoy@cps.edu>
2026-03-23 11:02:13 -0500

    fixed reject mode because we didn't put it back in

---
+++ JaBaDaBi/src/main/java/frc/robot/commands/swervedrive/subsystem/Reject.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.swervedrive.subsystem;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Loader;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Collector;
+
+public class Reject extends SequentialCommandGroup {
+    public Reject(Collector collector, Loader loader, Conveyor conveyor, Shooter shooter) {
+        addRequirements(collector, loader, conveyor, shooter);
+        addCommands(
+                collector.deploy(() -> collector.HALF_WAY_SET_POINT),
+                new ParallelCommandGroup(
+                        collector.intakeSetMotorPower(() -> -1),
+                        collector.deploy(() -> 0.33),
+                        loader.runLoader(() -> -0.7),
+                        conveyor.unloadFuel(() -> true)),
+                        shooter.shoot(() -> -50)
+                );
+    }
+}
